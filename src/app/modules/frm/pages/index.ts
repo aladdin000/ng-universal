164a9fb060a4d3836b59389b95579a1d3de4138e
Maxim Dobrolinsky <maxim_dobrolinsky@smartexlab.com>
2018-12-06 16:45:40 +0300

    add frm/program-and-exams

--- src/app/modules/frm/pages/index.ts
+++ src/app/modules/frm/pages/index.ts
@@ -1,3 +1,4 @@
 export * from '../frm.page';
 export * from './overview/overview.page';
 export * from './fees-payments/fees-payments.page';
+export * from './program-and-exams/program-and-exams.page';
