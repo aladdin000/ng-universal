0ae81f3831294648f158ce55621aec67e82addd1
Maxim Dobrolinsky <maxim_dobrolinsky@smartexlab.com>
2018-12-06 19:40:33 +0300

    add frm/fees-payments page

--- src/app/modules/shared/components/index.ts
+++ src/app/modules/shared/components/index.ts
@@ -3,3 +3,4 @@ export * from './slide/slide.component';
 export * from './conversation/conversation.component';
 export * from './plan-box/plan-box.component';
 export * from './line-title/line-title.component';
+export * from './price-box/price-box.component';
---
+++ src/app/modules/shared/components/price-box/price-box.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-price-box',
+  templateUrl: './price-box.component.html',
+  styleUrls: ['./price-box.component.scss'],
+})
+export class PriceBoxComponent {
+    @Input() public title: string;
+    @Input() public startDate: string;
+    @Input() public endDate: string;
+    @Input() public price: string;
+    @Input() public enrollmentFee: string;
+    @Input() public examFee: string;
+    @Input() public link: string;
+    @Input() public disabled: boolean;
+}
