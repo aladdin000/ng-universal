1e6749083465a56981841c2c1f99b6318eb039a7
Maxim Dobrolinsky <maximdobrolinsky@Maxims-MacBook-Pro.local>
2018-12-06 23:53:14 +0300

    add meta data

--- src/app/modules/frm/pages/fees-payments/fees-payments.page.ts
+++ src/app/modules/frm/pages/fees-payments/fees-payments.page.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { LINE_TITLE_TYPES } from '../../../shared/models/line-title-types';
+import { Title, Meta } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { MetaHelper } from 'src/app/modules/shared/helpers/meta.helper';
 
 @Component({
   templateUrl: './fees-payments.page.html',
   styleUrls: ['./fees-payments.page.scss'],
 })
-export class FeesPaymentsPage {
+export class FeesPaymentsPage extends MetaHelper implements OnInit {
   public lineTitleTypes = LINE_TITLE_TYPES;
+
+  constructor(
+    private route: ActivatedRoute,
+    title: Title,
+    meta: Meta,
+  ) {
+    super(title, meta);
+  }
+
+  ngOnInit() {
+    this.metaData = this.route.snapshot.data.meta;
+    this.setMetaData();
+  }
 }
--- src/app/modules/frm/pages/program-and-exams/program-and-exams.page.ts
+++ src/app/modules/frm/pages/program-and-exams/program-and-exams.page.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { LINE_TITLE_TYPES } from '../../../shared/models/line-title-types';
+import { MetaHelper } from 'src/app/modules/shared/helpers/meta.helper';
+import { ActivatedRoute } from '@angular/router';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   templateUrl: './program-and-exams.page.html',
   styleUrls: ['./program-and-exams.page.scss'],
 })
-export class ProgramAndExamsPage {
+export class ProgramAndExamsPage extends MetaHelper implements OnInit {
   public lineTitleTypes = LINE_TITLE_TYPES;
+
+  constructor(
+    private route: ActivatedRoute,
+    title: Title,
+    meta: Meta,
+  ) {
+    super(title, meta);
+  }
+
+  ngOnInit() {
+    this.metaData = this.route.snapshot.data.meta;
+    this.setMetaData();
+  }
 }
